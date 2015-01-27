0c7656c4ff173b9a8a019df587dcf1bfbee636bc
jishen.yjs <jishen.yjs@alibaba-inc.com>
2015-01-27 19:11:40 +0800

    add kline

---
+++ key-tools/src/main/java/com/key/tools/stock/pojo/KLine.java
@@ -0,0 +1,127 @@
+package com.key.tools.stock.pojo;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.key.tools.stock.db.model.StockHistory;
+
+public class KLine
+{
+	private Date date;
+
+	private Double open;
+
+	private Double close;
+
+	private Double high;
+
+	private Double low;
+
+	private Integer cycle;
+
+	public void init(List<StockHistory> list, int index, int cycle, Date date)
+	{
+		if (cycle <= 0)
+		{
+			return;
+		}
+		setCycle(cycle);
+
+		double open;
+
+		double close;
+
+		double high = 0;
+
+		double low;
+		open = list.get(index).getOpen();
+		high = list.get(index).getHigh();
+		low = list.get(index).getLow();
+
+		int end = index + cycle - 1;
+		if (end > list.size())
+		{
+			end = list.size();
+		}
+
+		for (int i = index; i <= end; i++)
+		{
+			StockHistory stockHistory = list.get(i);
+			if (stockHistory.getHigh() > high)
+			{
+				high = stockHistory.getHigh();
+			}
+			if (stockHistory.getLow() < low)
+			{
+				low = stockHistory.getLow();
+			}
+		}
+		close = list.get(end).getClose();
+		setOpen(open);
+		setClose(close);
+		setLow(low);
+		setHigh(high);
+
+	}
+
+	public Date getDate()
+	{
+		return date;
+	}
+
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
+
+	public Double getOpen()
+	{
+		return open;
+	}
+
+	public void setOpen(Double open)
+	{
+		this.open = open;
+	}
+
+	public Double getClose()
+	{
+		return close;
+	}
+
+	public void setClose(Double close)
+	{
+		this.close = close;
+	}
+
+	public Double getHigh()
+	{
+		return high;
+	}
+
+	public void setHigh(Double high)
+	{
+		this.high = high;
+	}
+
+	public Double getLow()
+	{
+		return low;
+	}
+
+	public void setLow(Double low)
+	{
+		this.low = low;
+	}
+
+	public Integer getCycle()
+	{
+		return cycle;
+	}
+
+	public void setCycle(Integer cycle)
+	{
+		this.cycle = cycle;
+	}
+
+}
